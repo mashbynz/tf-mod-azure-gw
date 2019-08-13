4c3c0ad49c402a168fb9bece7b904db5fa85b325
Matt Ashby <matt.ashby@theinstillery.com>
2019-08-13 13:43:54 +1200

    -ergw module

--- label.tf
+++ label.tf
@@ -22,11 +22,3 @@ module "ipconfig_label" {
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
 }
-
-module "ergw_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.14.1"
-  context    = var.context
-  attributes = ["ergw"]
-  delimiter  = "-"
-  additional_tag_map = {} /* Additional attributes (e.g. 1) */
-}
