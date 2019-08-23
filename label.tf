a3b895e29dff4926fce090b2cd9874811ad37e28
callumccr1 <callumccr@gmail.com>
2019-08-23 13:27:14 +1200

    Test

--- label.tf
+++ label.tf
@@ -18,6 +18,7 @@ module "er_label" {
   source             = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.14.1"
   context            = var.context
   attributes         = ["er"]
+  label_order        = ["name"]
   delimiter          = "-"
   additional_tag_map = {} /* Additional attributes (e.g. 1) */
 }
